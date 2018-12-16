a05c9e7339ee6f046084aa2b1c162bbe7a3cfb06
test <test1>
2018-12-16 14:47:06 +0900

    약관 동의 화면 그리기

--- app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/LoginActivity.java
+++ app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/LoginActivity.java
@@ -1,9 +1,14 @@
 package kr.tjeit.a20181216_02_loginandsignup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends BaseActivity {
+
+    TextView signUpTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,15 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
-
+        signUpTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                이동시작점과 도착점
+                Intent intent = new Intent(mContext, SignUpActivity.class);
+//                시작
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -27,6 +40,6 @@ public class LoginActivity extends BaseActivity {
 
     @Override
     public void bindViews() {
-
+        signUpTxt = findViewById(R.id.signUpTxt);
     }
 }
