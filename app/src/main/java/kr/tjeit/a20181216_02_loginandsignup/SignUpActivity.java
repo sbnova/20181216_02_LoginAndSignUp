c630c54afab0ba0d3e8020ecbf2b1bf5ce8617f3
test <test1>
2018-12-16 14:18:22 +0900

    로그인 화면 그리기 완료, 회원가입 화면 생성

---
+++ app/src/main/java/kr/tjeit/a20181216_02_loginandsignup/SignUpActivity.java
@@ -0,0 +1,31 @@
+package kr.tjeit.a20181216_02_loginandsignup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
